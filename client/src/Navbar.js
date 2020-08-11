a174bcd9f0f307364031274e55816f559d7593f6
Ildana <ildana.ruzybayeva@gmail.com>
2020-08-11 18:00:44 +0000

    Toggle Theme feature

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -13,19 +13,21 @@ import { ThemeContext } from "./contexts/ThemeContext";
 class Navbar extends Component {
   static contextType = ThemeContext;
   render() {
-    const { isDarkMode } = this.context;
+    const { isDarkMode, toggleTheme } = this.context;
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <AppBar position="static" color={isDarkMode ? "primary" : "default"}>
+        <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
           <Toolbar>
             <IconButton className={classes.menuButton} color="inherit">
-              <span>🇫🇷</span>
+              <span role="img" aria-label="france">
+                🇫🇷
+              </span>
             </IconButton>
             <Typography className={classes.title} variant="h6" color="inherit">
               App Title
             </Typography>
-            <Switch />
+            <Switch onChange={toggleTheme} />
             <div className={classes.grow} />
             <div className={classes.search}>
               <div className={classes.searchIcon}>
